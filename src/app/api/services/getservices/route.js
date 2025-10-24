ff933d4fa697c342cc835b8cd7c3c2839cfae9e7
Muqaddas12 <muqaddasmalik781@gmail.com>
2025-10-24 13:01:09 +0530

    last

--- src/app/api/services/getservices/route.js
+++ src/app/api/services/getservices/route.js
@@ -3,6 +3,5 @@ import { getServices } from "@/lib/services";
 
 export async function GET(request){
     const services= await getServices()
-    console.log(services)
     return Response.json(services)
 }
