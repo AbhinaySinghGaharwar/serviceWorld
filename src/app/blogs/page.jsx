de63a508499781ac83d552093ffa0fc31562f0d8
Muqaddas12 <muqaddasmalik781@gmail.com>
2025-11-26 12:14:32 +0530

    ab shi hai

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,6 +1,6 @@
 export default function AdminBlogList() {
   return (
-    <div className="p-6 text-gray-700 dark:text-gray-400">
+    <div className="min-h-screen p-6 text-gray-700 dark:text-gray-400">
       Blog list page (Coming soon...)
     </div>
   );
